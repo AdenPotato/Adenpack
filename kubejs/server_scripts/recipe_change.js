334b2c9d34d83692987f9842a884cca140910954
Aden <adenkappa403@gmail.com>
2026-05-15 20:05:21 -0600

    Version bump, added stuff

--- kubejs/server_scripts/recipe_change.js
+++ kubejs/server_scripts/recipe_change.js
@@ -9,7 +9,24 @@ ServerEvents.recipes(event => {
     event.remove({id: "onlyhammersandexcavators:lapis_excavator"})
     event.remove({id: "onlyhammersandexcavators:obsidian_excavator"})
     event.remove({id: "onlyhammersandexcavators:emerald_excavator"})
+
+    event.remove({id: 'extended_industrialization:tool/nano_suit_helmet_quantum_upgrade'})
+    event.remove({id: 'extended_industrialization:tool/nano_suit_leggings_quantum_upgrade'})
+    event.remove({id: 'extended_industrialization:tool/nano_suit_boots_quantum_upgrade'})
+    event.remove({id: 'extended_industrialization:tool/nano_suit_chestplate_quantum_upgrade'})
+    event.remove({id: 'extended_industrialization:tool/nano_saber_quantum_upgrade'})
+
+    event.remove({id: 'modern_industrialization:upgrade/packer/quantum/sword'})
+    event.remove({id: 'modern_industrialization:upgrade/packer/quantum/leggings'})
+    event.remove({id: 'modern_industrialization:upgrade/packer/quantum/chestplate'})
+    event.remove({id: 'modern_industrialization:upgrade/packer/quantum/leggings'})
+    event.remove({id: 'modern_industrialization:upgrade/packer/quantum/boots'})
+
+    event.remove({id: 'functionalstorage:obsidian_upgrade'})
+    event.remove({id: 'functionalstorage:water_generator_upgrade'})
+    event.remove({id: 'functionalstorage:dripping_upgrade'})
     
+
     event.shapeless(
         Item.of('minecraft:bamboo_planks', 2),
         [
